b867df952a11491ac0e5dcc3992baf164918c015
devalc <deva4998@vandals.uidaho.edu>
2022-03-16 10:55:01 -0700

    fixed download directory specification. Previously project was being downloaded  to the working directory instead of the specified path

--- scripts/wget_download_weppcloud_project.bat
+++ scripts/wget_download_weppcloud_project.bat
@@ -1,4 +1,4 @@
 echo downloading %1 %2
 wget -O- -v https://wepp.cloud/weppcloud/runs/%1/cfg/export/arcmap/?no_retrieve
 wget -O- -v https://wepp.cloud/weppcloud/runs/%1/cfg/export/prep_details/?no_retrieve
-wget -rv -nH --cut-dirs=5 --directory-prefix=%1 --header="raw: True"  https://wepp.cloud/weppcloud/runs/%1/cfg/browse/ %2
+wget -rv -nH --cut-dirs=5 -p --directory-prefix=%2 --header="raw: True"  https://wepp.cloud/weppcloud/runs/%1/cfg/browse/ 
