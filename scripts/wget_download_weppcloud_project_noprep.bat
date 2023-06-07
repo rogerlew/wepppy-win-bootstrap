2e0115c9c5c5718f148a799daad9e6a0a8f28020
Roger Lew <rogerlew@gmail.com>
2023-06-07 08:23:15 -0700

    updated download scripts

--- scripts/wget_download_weppcloud_project_noprep.bat
+++ scripts/wget_download_weppcloud_project_noprep.bat
@@ -1,3 +1,2 @@
 echo downloading %1 %2
-wget.exe -O- -v https://wepp.cloud/weppcloud/runs/%1/cfg/export/prep_details/?no_retrieve
 wget.exe -rv -nH --cut-dirs=5 -p --directory-prefix=%2 --header="raw: True"  https://wepp.cloud/weppcloud/runs/%1/cfg/browse/ 
